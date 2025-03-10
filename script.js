508912cd26d5a3eee6fb5da594338d211896dc1c
tathapadilha <145500493+tathapadilha@users.noreply.github.com>
2025-03-10 09:18:04 -0300

    diminuir fonte

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded',function(){
     const aumentaFonteBotao = document.getElementById('aumetar-font');
+   
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function( ){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = ${tamanhoAtualFonte}
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+    diminuirFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        
     })
 })
