de2a98470f52fbbe1536a24d1d8c5417c6deb677
tathapadilha <145500493+tathapadilha@users.noreply.github.com>
2025-03-31 10:41:30 -0300

    novo

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded',function(){
-    const aumentaFonteBotao = document.getElementById('aumetar-font');
-   
+    const aumentaFonteBotao = document.getElementById('aumenta-fonte');
+    const diminuiFonteBotao = document.getElementById('diminui-fonte');
     let tamanhoAtualFonte = 1;
-    aumentaFonteBotao.addEventListener('click', function( ){
-        tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
-    });
-
-    diminuirFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-        
-    })
-})
+     aumentaFonteBotao.addEventListener('click',function(){
+       tamanhoAtualFonte += 0,1;
+       document.body.style.fontSize = `$(tamanhoAtualFonte)rem`;
+     });
+    
+     let tamanhoAtualFonte = 1;
+      diminuiFonteBotao.addEventListener('click',function(){
+        tamanhoAtualFonte -= 0,1;
+        document.body.style.fontSize = `$(tamanhoAtualFonte)rem`;
+      });
+     });
